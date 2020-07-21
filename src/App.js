b0858c948016221f78b4e013852596749aff6d24
Jan van Hest <janvanhest@outlook.com>
2020-07-22 01:07:58 +0200

    Added simpel functional component about

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Intro from "./components/Intro.jsx";
 import Portfolio from "./components/Portfolio.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   return (
@@ -10,22 +11,9 @@ function App() {
       <Intro />
       <Portfolio />
       <About />
-      <Contact />
-      <h2>About me.</h2>
-      <p>
-        I am a junior developer and completed my education at Codaisseur. I
-        seize opportunities, look at options, solve problems and think in
-        solutions.
-      </p>
-      <p>
-        I listen carefully to others and make the impossible possible. I work
-        goal-oriented, seek clarity to work effectively and efficiently and am
-        not afraid to use tools.
-      </p>
-      <p>
-        Characteristics of me are ambition, an inquisitive learning attitude,
-        knowledge of people through experience, cooperation and independence.
-      </p>
+      {/* <About />
+      <Contact /> */}
+
       <p>Hobby's - Contact.</p>
     </div>
   );
