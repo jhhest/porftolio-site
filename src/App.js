5408ea2cab60515d3c74ed77c99778a28ecb3cc4
Jan van Hest <janvanhest@outlook.com>
2020-07-22 01:15:42 +0200

    Added function component contact

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header.jsx";
 import Intro from "./components/Intro.jsx";
 import Portfolio from "./components/Portfolio.jsx";
 import About from "./components/About.jsx";
-
+import Contact from "./components/Contact.jsx";
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
       <Intro />
       <Portfolio />
       <About />
+      <Contact />
     </div>
   );
 }
