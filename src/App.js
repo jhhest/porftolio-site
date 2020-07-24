e0583299c1806def3eaa9f6f0a90aa4892fc7c19
Jan van Hest <janvanhest@outlook.com>
2020-07-24 03:01:21 +0200

    Gave the intro some 80 styled heading.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 import Header from "./components/Header.jsx";
 import Intro from "./components/Intro.jsx";
 import Portfolio from "./components/Portfolio.jsx";
-import About from "./components/About.jsx";
-import Contact from "./components/Contact.jsx";
-
 
 function App() {
   return (
