7f2679bc35dfd0d7dd735395e662505317977613
Jan van Hest <janvanhest@outlook.com>
2020-07-26 00:39:50 +0200

    Extended contactdetails with tel.number and socialmedia accounts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,28 @@ function Contact() {
       <h2>
         <i class="fa fa-address-card-o" aria-hidden="true"></i> Contact
       </h2>
-      <p>ContactDetails</p>
+      <p>
+        <i class="fa fa-coffee" aria-hidden="true"></i> Let's stay in contact!
+        You are alway's welcome for a cup of coffee or just a talk. You can
+        contact me by social media, email or telephone. Just give a call. It is
+        way easier!
+      </p>
+      <p>
+        <i class="fa fa-mobile" aria-hidden="true"></i>: 0642091421
+      </p>
+      <p>
+        <i class="fa fa-envelope" aria-hidden="true"></i>:
+        janvanhest@outlook.com
+      </p>
+      <p>
+        <i class="fa fa-linkedin" aria-hidden="true"></i>:
+        https://www.linkedin.com/in/jan-developer/
+      </p>
+      <p>
+        <i class="fa fa-github" aria-hidden="true"></i>
+        :https://github.com/jhhest
+      </p>
     </section>
   );
 }
-
 export default Contact;
