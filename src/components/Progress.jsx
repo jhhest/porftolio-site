d01f3902e90878a5fd14c2f11b214d00b2f89382
Jan van Hest <janvanhest@outlook.com>
2020-07-26 03:47:52 +0200

    Gave timeline milestones rounded corners and a section with used technologies. For each technologie there is an respresentative icon

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import reactLogo from "../../src/tech-logo/reactjs.svg";
+import cssLogo from "../../src/tech-logo/css.svg";
+import htmlLogo from "../../src/tech-logo/html.svg";
+import ubuntuLogo from "../../src/tech-logo/ubuntu.svg";
+import dockerLogo from "../../src/tech-logo/docker.svg";
+import gitLogo from "../../src/tech-logo/git.svg";
+import javascriptLogo from "../../src/tech-logo/js_official.svg";
+import netlifyLogo from "../../src/tech-logo/netlify.svg";
+import nodeLogo from "../../src/tech-logo/node.svg";
+import npmLogo from "../../src/tech-logo/npm.svg";
+import postgreLogo from "../../src/tech-logo/postgre.svg";
 const Portfolio = () => {
   return (
     <section id="progress">
@@ -14,19 +24,32 @@ const Portfolio = () => {
               <h3>
                 <i class="fa fa-calendar-o" aria-hidden="true"></i> 01-01-2020{" "}
                 <i class="fa fa-chevron-right" aria-hidden="true"></i>{" "}
-                <span class="codaisseur">
-                  {":"} {"{"} {")"}
-                </span>{" "}
-                Codaisseur bootcamp{" "}
+                <span class="codaisseur">{": { )"}</span> Codaisseur bootcamp{" "}
               </h3>
               <p>
                 <span class="freeCodeCamp">
                   <i class="fa fa-info " aria-hidden="true"></i>
                 </span>{" "}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Doloribus sunt id corporis ut eos nisi aspernatur eaque vitae
-                ipsam iusto.
+                Intensive 3 month coding bootcamp in amsterdam. Over 40 hours a
+                week coding. Basically no live, just study.
               </p>
+              <h3>
+                <i class="fa fa-certificate" aria-hidden="true"></i> Used
+                Technologies
+              </h3>
+              <section class="usedTechnologies">
+                <img class="logo" src={reactLogo} alt="" />
+                <img class="logo" src={javascriptLogo} alt="" />
+                <img class="logo" src={htmlLogo} alt="" />
+                <img class="logo" src={cssLogo} alt="" />
+                <img class="logo" src={netlifyLogo} alt="" />
+                <img class="logo" src={ubuntuLogo} alt="" />
+                <img class="logo" src={dockerLogo} alt="" />
+                <img class="logo" src={gitLogo} alt="" />
+                <img class="logo" src={nodeLogo} alt="" />
+                <img class="logo" src={npmLogo} alt="" />
+                <img class="logo" src={postgreLogo} alt="" />
+              </section>
             </div>
           </li>
 
@@ -36,13 +59,17 @@ const Portfolio = () => {
                 <i class="fa fa-calendar-o" aria-hidden="true"></i> 01-01-2020{" "}
                 <i class="fa fa-chevron-right" aria-hidden="true"></i>{" "}
                 <i class="fa fa-free-code-camp" aria-hidden="true"></i>{" "}
-                Certificate - Responsive Webdesign{" "}
+                freeCodeCamp - Responsive Webdesign Certficate{" "}
               </h3>
               <p>
                 <i class="fa fa-info" aria-hidden="true"></i> Lorem ipsum dolor
                 sit amet, consectetur adipisicing elit. Doloribus sunt id
                 corporis ut eos nisi aspernatur eaque vitae ipsam iusto.
               </p>
+              <section class="usedTechnologies">
+                <img class="logo" src={htmlLogo} alt="" />
+                <img class="logo" src={cssLogo} alt="" />
+              </section>
             </div>
           </li>
           <li>
@@ -58,6 +85,11 @@ const Portfolio = () => {
                 sit amet, consectetur adipisicing elit. Doloribus sunt id
                 corporis ut eos nisi aspernatur eaque vitae ipsam iusto.
               </p>
+              <section class="usedTechnologies">
+                <img class="logo" src={htmlLogo} alt="" />
+                <img class="logo" src={cssLogo} alt="" />
+                <img class="logo" src={javascriptLogo} alt="" />
+              </section>
             </div>
           </li>
         </ul>
