f87e72f62b4aa7d8e398545211a16b387dc6f38a
Jan van Hest <janvanhest@outlook.com>
2020-07-28 00:25:00 +0200

    The Logo's are imported as reactComponents

--- src/components/progress/Milestone.jsx
+++ src/components/progress/Milestone.jsx
@@ -1,36 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
-import cssLogo from "../../../src/tech-logo/css.svg";
-import dockerLogo from "../../../src/tech-logo/docker.svg";
-import gitLogo from "../../../src/tech-logo/git.svg";
-import htmlLogo from "../../../src/tech-logo/html.svg";
-import javascriptLogo from "../../../src/tech-logo/js_official.svg";
-import netlifyLogo from "../../../src/tech-logo/netlify.svg";
-import nodeLogo from "../../../src/tech-logo/node.svg";
-import npmLogo from "../../../src/tech-logo/npm.svg";
-import postgreLogo from "../../../src/tech-logo/postgreSQL.svg";
-import reactLogo from "../../../src/tech-logo/reactjs.svg";
-import ubuntuLogo from "../../../src/tech-logo/ubuntu.svg";
-
-// <img class="logo" src={reactLogo} alt="" />
-// <img class="logo" src={javascriptLogo} alt="" />
-// <img class="logo" src={htmlLogo} alt="" />
-// <img class="logo" src={cssLogo} alt="" />
-// <img class="logo" src={netlifyLogo} alt="" />
-// <img class="logo" src={ubuntuLogo} alt="" />
-// <img class="logo" src={dockerLogo} alt="" />
-// <img class="logo" src={gitLogo} alt="" />
-// <img class="logo" src={nodeLogo} alt="" />
-// <img class="logo" src={npmLogo} alt="" />
-// <img class="logo" src={postgreSQLLogo} alt="" />
+import cssLogo from "../../tech-logo/css.svg";
+import reactLogo from "../../tech-logo/reactjs.svg";
+import dockerLogo from "../../tech-logo/docker.svg";
+import gitLogo from "../../tech-logo/git.svg";
+import htmlLogo from "../../tech-logo/html.svg";
+import javascriptLogo from "../../tech-logo/js_official.svg";
+import netlifyLogo from "../../tech-logo/netlify.svg";
+import nodeLogo from "../../tech-logo/node.svg";
+import npmLogo from "../../tech-logo/npm.svg";
+import postgreSQLLogo from "../../tech-logo/postgreSQL.svg";
+import ubuntuLogo from "../../tech-logo/ubuntu.svg";
 
 function Milestone(props) {
-  const { description, title } = props;
-  const logoUrl = {};
-
-  const returnTechnologieLogo = () => {
-    return <img class="logo" src={reactLogo} alt="" />;
-  };
+  const { description, title, UsedTechnologies } = props;
+  console.log(typeof reactLogo);
+  const returnLogos = (logos) => logos.map((technologie) => technologie);
   return (
     <motion.li
       initial={{
@@ -42,19 +27,23 @@ function Milestone(props) {
     >
       <div>
         <h3>
-          <i class="fa fa-calendar-o" aria-hidden="true"></i> 01-01-2020{" "}
-          <i class="fa fa-chevron-right" aria-hidden="true"></i> {title}
+          <i className="fa fa-calendar-o" aria-hidden="true"></i> 01-03-2020{" "}
+          <i className="fa fa-chevron-right" aria-hidden="true"></i> {title}
         </h3>
         <p>
-          <span class="freeCodeCamp">
-            <i class="fa fa-info " aria-hidden="true"></i>
+          <span className="freeCodeCamp">
+            <i className="fa fa-info " aria-hidden="true"></i>
           </span>{" "}
           {description}
         </p>
         <h3>
-          <i class="fa fa-certificate" aria-hidden="true"></i> Used Technologies
+          <i className="fa fa-certificate" aria-hidden="true"></i> Used
+          Technologies
         </h3>
-        <section class="usedTechnologies">{returnTechnologieLogo()}</section>
+
+        <section className="usedTechnologies">
+          {returnLogos(UsedTechnologies)}
+        </section>
       </div>
     </motion.li>
   );
--- src/components/progress/Progress.jsx
+++ src/components/progress/Progress.jsx
@@ -1,60 +1,54 @@
 import React from "react";
-import cssLogo from "../../../src/tech-logo/css.svg";
-import dockerLogo from "../../../src/tech-logo/docker.svg";
-import gitLogo from "../../../src/tech-logo/git.svg";
-import htmlLogo from "../../../src/tech-logo/html.svg";
-import javascriptLogo from "../../../src/tech-logo/js_official.svg";
-import netlifyLogo from "../../../src/tech-logo/netlify.svg";
-import nodeLogo from "../../../src/tech-logo/node.svg";
-import npmLogo from "../../../src/tech-logo/npm.svg";
-import postgreSQLLogo from "../../../src/tech-logo/postgreSQL.svg";
-import reactLogo from "../../../src/tech-logo/reactjs.svg";
-import ubuntuLogo from "../../../src/tech-logo/ubuntu.svg";
+import { ReactComponent as UbuntuLogo } from "../../../src/tech-logo/ubuntu.svg";
+import { ReactComponent as CssLogo } from "../../../src/tech-logo/css.svg";
+import { ReactComponent as DockerLogo } from "../../../src/tech-logo/docker.svg";
+import { ReactComponent as GitLogo } from "../../../src/tech-logo/git.svg";
+import { ReactComponent as HtmlLogo } from "../../../src/tech-logo/html.svg";
+import { ReactComponent as JavaScriptLogo } from "../../../src/tech-logo/js_official.svg";
+import { ReactComponent as NetlifyLogo } from "../../../src/tech-logo/netlify.svg";
+import { ReactComponent as NodeLogo } from "../../../src/tech-logo/node.svg";
+import { ReactComponent as NpmLogo } from "../../../src/tech-logo/npm.svg";
+import { ReactComponent as PostgreSQLLogo } from "../../../src/tech-logo/postgreSQL.svg";
+import { ReactComponent as ReactLogo } from "../../../src/tech-logo/reactjs.svg";
 import Milestone from "./Milestone";
 
 const Portfolio = () => {
   return (
     <section id="skills">
       <h2>
-        <i class="fa fa-graduation-cap" aria-hidden="true"></i> Experience and
-        Skills
+        <i className="fa fa-graduation-cap" aria-hidden="true"></i> Experience
+        and Skills
       </h2>
       <section id="timeline">
         <ul>
           <Milestone
+            date={new Date(2020, 2, 1)}
             title="freeCodeCamp Front-end certificate"
             description="300 Hours of coursework. Projects working on bootstrap, sass, lorem10"
+            UsedTechnologies={[<ReactLogo />, <UbuntuLogo />]}
           />
           <li>
             <div>
               <h3>
-                <i class="fa fa-calendar-o" aria-hidden="true"></i> 01-01-2020{" "}
-                <i class="fa fa-chevron-right" aria-hidden="true"></i>{" "}
-                <span class="codaisseur">{": { )"}</span> Codaisseur bootcamp{" "}
+                <i className="fa fa-calendar-o" aria-hidden="true"></i>{" "}
+                01-01-2020{" "}
+                <i className="fa fa-chevron-right" aria-hidden="true"></i>{" "}
+                <span className="codaisseur">{": { )"}</span> Codaisseur
+                bootcamp{" "}
               </h3>
               <p>
-                <span class="freeCodeCamp">
-                  <i class="fa fa-info " aria-hidden="true"></i>
+                <span className="freeCodeCamp">
+                  <i className="fa fa-info " aria-hidden="true"></i>
                 </span>{" "}
                 Intensive 3 month coding bootcamp in amsterdam. Over 40 hours a
                 week coding. Basically no live, just study.
               </p>
               <h3>
-                <i class="fa fa-certificate" aria-hidden="true"></i> Used
+                <i className="fa fa-certificate" aria-hidden="true"></i> Used
                 Technologies
               </h3>
-              <section class="usedTechnologies">
-                <img class="logo" src={reactLogo} alt="" />
-                <img class="logo" src={javascriptLogo} alt="" />
-                <img class="logo" src={htmlLogo} alt="" />
-                <img class="logo" src={cssLogo} alt="" />
-                <img class="logo" src={netlifyLogo} alt="" />
-                <img class="logo" src={ubuntuLogo} alt="" />
-                <img class="logo" src={dockerLogo} alt="" />
-                <img class="logo" src={gitLogo} alt="" />
-                <img class="logo" src={nodeLogo} alt="" />
-                <img class="logo" src={npmLogo} alt="" />
-                <img class="logo" src={postgreSQLLogo} alt="" />
+              <section className="usedTechnologies">
+                <ReactLogo />
               </section>
             </div>
           </li>
@@ -62,39 +56,38 @@ const Portfolio = () => {
           <li>
             <div>
               <h3>
-                <i class="fa fa-calendar-o" aria-hidden="true"></i> 01-01-2020{" "}
-                <i class="fa fa-chevron-right" aria-hidden="true"></i>{" "}
-                <i class="fa fa-free-code-camp" aria-hidden="true"></i>{" "}
+                <i className="fa fa-calendar-o" aria-hidden="true"></i>{" "}
+                01-01-2020{" "}
+                <i className="fa fa-chevron-right" aria-hidden="true"></i>{" "}
+                <i className="fa fa-free-code-camp" aria-hidden="true"></i>{" "}
                 freeCodeCamp - Responsive Webdesign Certficate{" "}
               </h3>
               <p>
-                <i class="fa fa-info" aria-hidden="true"></i> Lorem ipsum dolor
-                sit amet, consectetur adipisicing elit. Doloribus sunt id
+                <i className="fa fa-info" aria-hidden="true"></i> Lorem ipsum
+                dolor sit amet, consectetur adipisicing elit. Doloribus sunt id
                 corporis ut eos nisi aspernatur eaque vitae ipsam iusto.
               </p>
-              <section class="usedTechnologies">
-                <img class="logo" src={htmlLogo} alt="" />
-                <img class="logo" src={cssLogo} alt="" />
+              <section className="usedTechnologies">
+                <ReactLogo />
               </section>
             </div>
           </li>
           <li>
             <div>
               <h3>
-                <i class="fa fa-calendar-o" aria-hidden="true"></i> 01-01-2020{" "}
-                <i class="fa fa-chevron-right" aria-hidden="true"></i>{" "}
-                <i class="fa fa-free-code-camp" aria-hidden="true"></i>{" "}
+                <i className="fa fa-calendar-o" aria-hidden="true"></i>{" "}
+                01-01-2020{" "}
+                <i className="fa fa-chevron-right" aria-hidden="true"></i>{" "}
+                <i className="fa fa-free-code-camp" aria-hidden="true"></i>{" "}
                 Certficate - JavaScript Algrorithms and Data Structures{" "}
               </h3>
               <p>
-                <i class="fa fa-info" aria-hidden="true"></i> Lorem ipsum dolor
-                sit amet, consectetur adipisicing elit. Doloribus sunt id
+                <i className="fa fa-info" aria-hidden="true"></i> Lorem ipsum
+                dolor sit amet, consectetur adipisicing elit. Doloribus sunt id
                 corporis ut eos nisi aspernatur eaque vitae ipsam iusto.
               </p>
-              <section class="usedTechnologies">
-                <img class="logo" src={htmlLogo} alt="" />
-                <img class="logo" src={cssLogo} alt="" />
-                <img class="logo" src={javascriptLogo} alt="" />
+              <section className="usedTechnologies">
+                <ReactLogo />
               </section>
             </div>
           </li>
