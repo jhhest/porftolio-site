260b0324e9dd5e04a37a1b11a97b985bcca31980
Jan van Hest <janvanhest@outlook.com>
2020-07-29 01:58:25 +0200

    Added undraw images to about and contact section. Both images are made responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,42 +1,46 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { ReactComponent as DeveloperProgrammingSVG } from "../images/programming.svg";
 
 function Contact() {
   return (
-    <motion.section
-      id="contact"
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-    >
-      <h2>
-        <i className="fa fa-address-card-o" aria-hidden="true"></i> Contact
-      </h2>
-      <p>
-        <i className="fa fa-coffee" aria-hidden="true"></i> Contact Let's keep
-        in touch! You are welcome for a cup of coffee or a nice conversation.
-        You can contact us via social media, email or telephone. And I prefer
-        just calling.
-      </p>
-      <p>
-        <i className="fa fa-mobile" aria-hidden="true"></i>: 0642091421
-      </p>
-      <p>
-        <i className="fa fa-envelope" aria-hidden="true"></i>:
-        janvanhest@outlook.com
-      </p>
-      <p>
-        <i className="fa fa-linkedin" aria-hidden="true"></i>:
-        https://www.linkedin.com/in/jan-developer/
-      </p>
-      <p>
-        <i className="fa fa-github" aria-hidden="true"></i>:
-        https://github.com/jhhest
-      </p>
-    </motion.section>
+    <section id="contact">
+      <motion.section
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        id="contact-info"
+      >
+        <h2>
+          <i className="fa fa-address-card-o" aria-hidden="true"></i> Contact
+        </h2>
+        <p>
+          <i className="fa fa-coffee" aria-hidden="true"></i> Let's keep in
+          touch! You are welcome for a cup of coffee or a nice conversation. You
+          can contact us via social media, email or telephone. And I prefer just
+          calling.
+        </p>
+        <p>
+          <i className="fa fa-mobile" aria-hidden="true"></i>: 0642091421
+        </p>
+        <p>
+          <i className="fa fa-envelope" aria-hidden="true"></i>:
+          janvanhest@outlook.com
+        </p>
+        <p>
+          <i className="fa fa-linkedin" aria-hidden="true"></i>:
+          https://www.linkedin.com/in/jan-developer/
+        </p>
+        <p>
+          <i className="fa fa-github" aria-hidden="true"></i>:
+          https://github.com/jhhest
+        </p>
+      </motion.section>
+      <DeveloperProgrammingSVG id="contact-image" />
+    </section>
   );
 }
 export default Contact;
--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { motion } from "framer-motion";
-
+import { ReactComponent as HobbySVG } from "../images/biking_kc4f.svg";
 function Hobbies() {
   return (
-    <motion.section
-      id="hobbies"
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-    >
-      <h2>
-        <i className="fa fa-gamepad" aria-hidden="true"></i> Hobby's
-      </h2>
-      <p>Diverse, characterized by technical and creative activities:</p>
-      <ul className="hobbies">
-        <li> Programming websites</li>
-        <li>
-          Work with graphics programs such as Gimp, Inkscape, Photoshop and
-          illustrator
-        </li>
-        <li>Electronics and 3D-printers</li>
-        <li>Digital photography</li>
-        <li>Bicycles</li>
-        <li>Nature</li>
-      </ul>
-    </motion.section>
+    <section id="hobbies">
+      <motion.section
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+      >
+        <h2>
+          <i className="fa fa-gamepad" aria-hidden="true"></i> Hobby's
+        </h2>
+        <p>Diverse, characterized by technical and creative activities:</p>
+        <ul className="hobbies">
+          <li> Programming websites</li>
+          <li>
+            Work with graphics programs such as Gimp, Inkscape, Photoshop and
+            illustrator
+          </li>
+          <li>Electronics and 3D-printers</li>
+          <li>Digital photography</li>
+          <li>Bicycles</li>
+          <li>Nature</li>
+        </ul>
+      </motion.section>
+        <HobbySVG id="hobbyimage"/>
+    </section>
   );
 }
 
